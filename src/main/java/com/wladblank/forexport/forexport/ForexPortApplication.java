3fc15b59585182acc5b98fd3ca39e6cebb2fd065
wladislavcugunov <wlad.c@me.com>
2020-10-25 23:47:08 +0100

    add jackson

--- src/main/java/com/wladblank/forexport/forexport/ForexPortApplication.java
+++ src/main/java/com/wladblank/forexport/forexport/ForexPortApplication.java
@@ -1,11 +1,15 @@
 package com.wladblank.forexport.forexport;
 
-import java.net.http.HttpResponse;
+import java.io.IOException;
 
-import org.springframework.boot.SpringApplication;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -17,11 +21,10 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class ForexPortApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		String url = "https://www.investing.com/instruments/Service/GetTechincalData";
 		//gives 1 minute, 5 minutes, 15 minutes, 30 minutes, 1 hour, 5 hours and 1 day array.
-		String xrequest= "X-Requested-With:XMLHttpRequest";
 		RestTemplate restTemplate = new RestTemplate();
 
 		HttpHeaders headers= new HttpHeaders();
@@ -37,10 +40,24 @@ public class ForexPortApplication {
 
 		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(formDataMap, headers);
 		ResponseEntity<String> response = restTemplate.postForEntity(url, request , String.class);
-		   
-		System.out.println(response.getStatusCode());
-		System.out.println(response.getBody());
-		  
+		
+		JacksonXmlModule xmlModule = new JacksonXmlModule();
+		xmlModule.setDefaultUseWrapper(false);
+		XmlMapper xmlMapper = new XmlMapper(xmlModule);
+	
+		JsonNode jsonNode = xmlMapper.readTree(response.getBody().getBytes());
+		ObjectMapper jsonMapper = new ObjectMapper();
+		jsonMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		String json = jsonMapper.writeValueAsString(jsonNode);
+	
+		// TODO: findout how to use propper mapping here
+
+		System.out.println("from Body:");
+		System.out.println(json);
+
+		//System.out.println(response.getBody());
+
+
 		//SpringApplication.run(ForexPortApplication.class, args);
 
 
